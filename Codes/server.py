f5574a8b14fc42add439ec3240cb8f31a2abe2a7
Param Raval <47474201+Param-Raval@users.noreply.github.com>
2020-03-15 13:29:37 +0530

    Add files via upload

---
+++ Codes/server.py
@@ -0,0 +1,33 @@
+import http.server
+import socketserver
+from our_chatbot import chatbot
+import pandas as pd
+import csv
+iter1 = 0
+PORT = 8080
+DIRECTORY = 'public'
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def do_POST(self):
+        global iter1
+        i=-1
+        self.send_response(200)
+        content_length = int(self.headers['Content-Length'])
+        post_body = self.rfile.read(content_length)
+        self.end_headers()
+        print('user query', post_body)
+        google_search_chatbot_reply = chatbot(post_body, iter1) #1
+        print(google_search_chatbot_reply)
+        iter1=iter1+1
+        self.wfile.write(str.encode(google_search_chatbot_reply))
+
+with socketserver.TCPServer(('', PORT), Handler) as httpd:
+    print('serving at port', PORT)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
